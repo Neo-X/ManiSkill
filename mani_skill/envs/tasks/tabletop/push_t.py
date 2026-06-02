57c7b337573f3d7b6b2644efe9ab108be0c770da
Neo-X <glen.berseth@mila.quebec>
2026-06-02 10:54:58 -0400

    Revert "Fix WhiteTableSceneBuilder crash when render_backend=none"
    This reverts commit b82b4f95cedffc94851acd4fed585d3fc0a548a4.

--- mani_skill/envs/tasks/tabletop/push_t.py
+++ mani_skill/envs/tasks/tabletop/push_t.py
@@ -48,10 +48,11 @@ class WhiteTableSceneBuilder(TableSceneBuilder):
         super().build()
         # cheap way to un-texture table
         for part in self.table._objs:
-            render_body = part.find_component_by_type(sapien.render.RenderBodyComponent)
-            if render_body is None or not render_body.render_shapes:
-                continue
-            for triangle in render_body.render_shapes[0].parts:
+            for triangle in (
+                part.find_component_by_type(sapien.render.RenderBodyComponent)
+                .render_shapes[0]
+                .parts
+            ):
                 triangle.material.set_base_color(np.array([255, 255, 255, 255]) / 255)
                 triangle.material.set_base_color_texture(None)
                 triangle.material.set_normal_texture(None)
